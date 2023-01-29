c4024bc845515207aaf1f3dc4e8ffc00f75a302d
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2023-01-29 23:39:27 +0530

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -4,11 +4,9 @@ $(document).ready(function (){
         var name=$("#name").val();
         var mobile=$("#mobile").val();
         var country=$("#country").val();
+        const userdata = localStorage.getItem('#userdata');
     
-        // localStorage.getItem("userdata", JSON.stringify(data));
-        // console.log(JSON.stringify(data));
-
-        if(name == "" || mobile == "" || country == "")
+        if( name == "" || mobile == "" || country == "")
         {
     $('#msg').html("Please fill all the details")
         }
@@ -21,20 +19,25 @@ $(document).ready(function (){
                                 {
                                     name:name,
                                     mobile:mobile,
-                                    country:country
+                                    country:country,
+                                    userdata:userdata
                 
                                 },
                 success: function (data) {
                     $('#msg').html(data);
-                    localStorage.getItem("userdata", JSON.stringify(data));
-                    console.log(JSON.stringify(data));
-                    location.href='http://localhost/developers/index.html';
+                   
                 },
     
             })
         }
     })
     })
-       
 
-   
+    $(document).ready(function (){
+        $(document).on("click", "#logout", function (e){
+            e.preventDefault();
+localStorage.getItem("userdata");
+localStorage.removeItem("userdata");
+location.href='http://localhost/developers/index.html';
+        })
+    })
