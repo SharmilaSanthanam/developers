586c3642807175b65c4253c3c8ed01d6fd31ca1f
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2023-01-29 23:40:09 +0530

    Add files via upload

--- php/profile.php
+++ php/profile.php
@@ -2,34 +2,42 @@
 require_once __DIR__ . '/vendor/autoload.php';
 $con = new MongoDB\Client("mongodb+srv://profile:profile@cluster0.7e1ierx.mongodb.net/?retryWrites=true&w=majority");
 
-// if(isset($_POST['create'])){
+echo " Welcome ";
+echo  $_POST['name'] . '<br>';
+
+require 'vendor/autoload.php';
+$redis = new Predis\Client();
+$cachedEntry = $redis->get('profile');
+
 if($_POST)
 {
     $db = $con->selectDatabase('Profiledb');
     $collection = $db->selectCollection('userdetails');
     
     $datas = [
-        // 'name' => 'test1',
-        // 'mobile' => '9876543212',
-        // 'country' => 'India',
-        'name' => $_POST['name'],
+   
         'mobile' => $_POST['mobile'],
         'country' => $_POST['country'],
     ];
    if($result = $collection->insertOne($datas))
    {
-    echo "Profile data successfully stored!!!";
+    // echo "Profile data successfully stored!!!" . '<br>';
+    $id = $result->getInsertedId();
+$temp = '';
+$temp .= $_POST['mobile'] . '<br>' . ' ' . $_POST['country'] . '<br>';
+
+$redis->set('profile', $temp);
+echo $cachedEntry;
+echo "To confirm your profile update:" . '<br>';
+exit();
    } 
    else{
     echo "Please try again after some time";
    }
-
-    $id = $result->getInsertedId();
-    
 }
-else{
+
+   else{
     echo "Please enter all the required data";
 }
-
 
 ?>
