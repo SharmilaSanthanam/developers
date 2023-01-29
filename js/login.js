7d674655eb6802044d97fccad1fff5c41a16474b
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2023-01-29 21:02:47 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,33 +1,23 @@
-$(document).ready(function(){  
-    $('#login_button').click(function(){
-var email = $("#email").val();  
-         var password = $("#password").val(); 
+$(document).ready(function () {
+    $('#login_button').click(function () {
+        var email = $("#email").val();
+        var password = $("#password").val();
 
-         var data = "email=" + email + "&password=" + password;
+        var data = "email=" + email + "&password=" + password;
 
-         $.ajax({ 
-            method:"post", 
-            url: "http://localhost/developers/php/login.php",  
+        $.ajax({
+            method: "post",
+            url: "http://localhost/developers/php/login.php",
             data: data,
-            // success: function(data){
-            //                    $("#msg").html(data);
-            //                    }
-                            
-        
-            success: function(data){
+            success: function (data) {
                 $("#msg").html(data);
-                if(data==""){
-                    // $("#msg").html(data);
-                    alert("Invalid Credentials");
-                }
-                else{
-                    localStorage.setItem("userdata", JSON.stringify(data));
-                    console.log(JSON.stringify(data));
-                    location.href='http://localhost/developers/profile.html';
-                }
-                              
+
+                localStorage.setItem("userdata", JSON.stringify(data));
+                console.log(JSON.stringify(userdata));
+               
+
             }
-        })      
+        })
 
     })
 })
